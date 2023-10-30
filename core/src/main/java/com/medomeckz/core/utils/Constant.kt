63c08e6b3142ca57c8b3307ca7e8886ab2b51bbf
albarrazikrillah14 <albarrazikrillah1405@gmail.com>
2023-10-31 01:47:10 +0700

    update

--- core/src/main/java/com/medomeckz/core/utils/Constant.kt
+++ core/src/main/java/com/medomeckz/core/utils/Constant.kt
@@ -2,5 +2,5 @@ package com.medomeckz.core.utils
 
 object Constant {
     const val BASE_URL = "https://api.github.com/"
-    const val KEY = "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"
+    const val KEY = "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"
 }
